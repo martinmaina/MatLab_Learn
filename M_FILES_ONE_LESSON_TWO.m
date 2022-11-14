bdf425ec2f06bc0521eebc2a39505a3ca74c071c
martinmaina <m0Lzixs3m0qy@gmail.com>
2022-11-14 19:19:18 +0300

    Matlab Practice

---
+++ M_FILES_ONE_LESSON_TWO.m
@@ -0,0 +1,13 @@
+y = cos(0)+sin(90)+2 * tan (45);
+x=log(150)+3*log10(6);
+z=exp(cos(90)) + sqrt(647967);
+t = 3 + 4i;
+v = conj(t) - (y * x) + z;
+%fprintf('%f \n', v);
+%disp(v);
+conjt_t=conj(t);
+%disp(conjt_t);
+varfunny = 'Gaka niko karurigi gakwa kambele matlabu-ini -:)';
+str2double(varfunny)
+string_number = '100.25';
+str2double(string_number)
