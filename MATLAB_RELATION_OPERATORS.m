5489c389c1be64d9ec445bdd1300578f2c42915f
martinmaina <m0Lzixs3m0qy@gmail.com>
2022-11-14 19:17:11 +0300

    Matlab Relational Operators

---
+++ MATLAB_RELATION_OPERATORS.m
@@ -0,0 +1,19 @@
+
+var_x = 100;
+var_y = 15;
+var_z = -25*pi;
+
+eq(var_x, var_y)
+
+ge(var_x, var_y)
+
+gt(var_x, var_y)
+
+le(var_z, var_y)
+
+lt(var_z, (var_x+var_y))
+
+
+ne(var_z, var_x)
+
+isequal(var_x, (var_y+85))
